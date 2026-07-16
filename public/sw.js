d8e38d80e966dff57aad5d44f42c88913b054e6d
Admin <admin@Mucha-MacBook.local>
2026-07-16 04:39:56 +0300

    Add killer service worker to destroy cached app versions

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', function(e) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    }).then(function() {
+      return self.registration.unregister();
+    })
+  );
+});
